74a37ef8250b04064bb17d548bfc59ce39e4880a
denoman4505 <98054759+denoman4505@users.noreply.github.com>
2022-01-20 00:35:48 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sa-op-jdms-iopa
+# DarkParadise
---
+++ settings.js
@@ -0,0 +1,59 @@
+const settings = {
+    "MongoDB": "mongodb+srv://deniz4505:deniz4505@cluster0.0wr6f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "commandChannel": ["919307472257314867"],
+    "taglıAlım": false,
+    "onsekizatilacakoda": "",
+    "onsekizodalar": ["931529237104570440","931529239428231229","931529341689528393","931529347511250984"],
+    "sorunCozucu": ["931529061698781244"],
+    "terapist": ["931529062592151582"],
+    "basvuruLog": "931529359238500372",
+    "chatMesajı": "-member-, Aramıza hoş geldiniz! Rol seçim odalarından rolleriniz almayı unutmayın iyi eğlenceler.",
+
+    "TAG_SYSTEM_CHANNEL": "931529363231502386",
+    "YETKI_VER_LOG": "931529288581283840",
+    "CEZA_PUAN_KANAL": "931529383217365062",
+    "CEZA_PUAN_SYSTEM": true,
+
+    // PUAN SİSTEMİ KURULUM
+    "CHAT_KANAL": "931529264392708116",
+    "PUBLIC_KATEGORI": "931529172843634768",
+    "STREAMER_KATEGORI": "931529171992182784",
+    "REGISTER_KATEGORI": "931529157718978560",
+    "SLEEP_ROOM": "931529273007824966",
+
+    // AUTO UPDATE KURULUM
+    "GörevSystem": {
+        "KanalID": "931529238488698900",
+        "MesajID": "931537860962709544"
+    },
+
+    "leaderboard": {
+        "KanalID": "",
+        "mesajID": "",
+        "mesajID_2": "",
+        "mesajID_3": "",
+        "mesajID_4": "",
+    },
+
+    "yetkilisay": {
+        "KanalID": "931529236337004564",
+        "MesajID": "931537985973911633"
+    },
+
+
+
+    "MODERASYON": "OTIxNzIwMjM5NDg4NjQ3MTY4.Yb3A3Q.I_pLYsxd1bVefW1RyQogMbT568o", // mod botu
+    "STATS": "OTI1MzYwMjQ4ODYzMjAzNDIw.Ycr-4w.vMhBTwUADYT8BHWQq_RA7hlhUeo", // stat
+    "EXECUTIVE": "OTI1MzU5MzE0MDg3MDU5NDY2.Ycr-BA.7V26Ee1HRogZ8eraO8V-_U1pXAk", // yönetim
+    "LOG": "OTI1MzU5ODMzNzIzNTg0NTMy.Ycr-gA.TOun18lO0a0KcclW1-dkg4ryZ3E", // log botu
+    "ASYNC": "OTI1MzU4NzYwNDQ3MzI4MzA2.Ycr9gA.rlvmikSrLwcEFvseTPUwfW7L96w", // async botu
+
+    "prefix": [".", "!"],
+    "botSesID": "933072903702970378",
+    "sunucuId": "931205053207953448",
+    "sahip": ["917303652325150741",""],
+    "footer": "Tagor ❤️ Balendin",
+    "readyFooter": ["Tagor ❤️ Balendin"]
+}
+
+module.exports = settings;
